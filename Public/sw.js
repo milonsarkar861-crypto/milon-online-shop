dabd598f1bf660cfca073bf3b595165e1bc82565
milonsarkar861-crypto <milonsarkar861@gmail.com>
2026-09-16 23:10:59 +0600

    Delete Public/sw.js

--- Public/sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
